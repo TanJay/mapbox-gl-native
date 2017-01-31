1e69a24d96ece4aed45eb20eb1bcbab2f535dde2
Jesse Bounds <jesse@rebounds.net>
2017-01-30 16:36:23 -0800

    wip: organize swift tests

--- platform/darwin/test/MGLStyleValueTests.swift
+++ platform/darwin/test/MGLStyleValueTests.swift
@@ -20,10 +20,23 @@ extension MGLStyleValueTests {
         symbolStyleLayer.text = MGLStyleConstantValue(rawValue: "{name}")
         XCTAssertEqual((symbolStyleLayer.text as! MGLStyleConstantValue<NSString>).rawValue, "{name}")
     }
-    
-    func testFunctions() {
+
+    func testConstants() {
         let shapeSource = MGLShapeSource(identifier: "test", shape: nil, options: nil)
-        let symbolStyleLayer = MGLSymbolStyleLayer(identifier: "test", source: shapeSource)
+        let circleStyleLayer = MGLCircleStyleLayer(identifier: "circleLayer", source: shapeSource)
+
+        var circleTranslationOne = CGVector(dx: 100, dy: 0)
+        let circleTranslationValueOne = NSValue(bytes: &circleTranslationOne, objCType: "{CGVector=dd}")
+
+        // non-data-driven constant
+        let expectedValue = MGLStyleValue<NSValue>(rawValue: circleTranslationValueOne)
+        circleStyleLayer.circleTranslation = expectedValue
+        XCTAssertEqual(circleStyleLayer.circleTranslation, expectedValue)
+    }
+
+    func testDeprecatedFunctions() {
+        let shapeSource = MGLShapeSource(identifier: "test", shape: nil, options: nil)
+        let symbolStyleLayer = MGLSymbolStyleLayer(identifier: "symbolLayer", source: shapeSource)
 
         // deprecated function, stops with float values
         let iconHaloBlurStyleValue = MGLStyleValue<NSNumber>(interpolationBase: 1.0, stops: [1: MGLStyleValue(rawValue: 0),
@@ -45,5 +58,23 @@ extension MGLStyleValueTests {
         // iconAllowsOverlap is boolean so mgl and mbgl conversions will coerce the developers stops into interval stops
         let expectedIconAllowsOverlapStyleValue = MGLStyleValue.cameraFunctionValue(with: .interval, stops: stops, options: nil)
         XCTAssertEqual(symbolStyleLayer.iconAllowsOverlap, expectedIconAllowsOverlapStyleValue)
+    }
+
+    func testFunctions() {
+        let shapeSource = MGLShapeSource(identifier: "test", shape: nil, options: nil)
+        let circleStyleLayer = MGLCircleStyleLayer(identifier: "circleLayer", source: shapeSource)
+
+        var circleTranslationOne = CGVector(dx: 100, dy: 0)
+        let circleTranslationValueOne = NSValue(bytes: &circleTranslationOne, objCType: "{CGVector=dd}")
+        var circleTranslationTwo = CGVector(dx: 0, dy: 0)
+        let circleTranslationValueTwo = NSValue(bytes: &circleTranslationTwo, objCType: "{CGVector=dd}")
+
+        let circleTranslationStops = [
+            0: MGLStyleValue(rawValue: circleTranslationValueOne),
+            10: MGLStyleValue(rawValue: circleTranslationValueTwo)
+        ]
+        let expectedCircleTranslationValue = MGLStyleValue<NSValue>.cameraFunctionValue(with: .interval, stops: circleTranslationStops, options: nil)
+        circleStyleLayer.circleTranslation = expectedCircleTranslationValue
+        XCTAssertEqual(circleStyleLayer.circleTranslation, expectedCircleTranslationValue)
     }
 }
